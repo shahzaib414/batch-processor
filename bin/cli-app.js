ac3f174347d7e1a06c4631f44e309b0e6c873d19
Shahzaib Shahid <shahzaib.shahid414@gmail.com>
2023-10-02 01:32:36 +0200

    refactor: Clean code / Validate args

--- bin/cli-app.js
+++ bin/cli-app.js
@@ -1,20 +1,26 @@
 import { readFile } from 'fs';
 import arg from '../lib/commands.js';
 import batchProcessor from '../lib/batchProcessor.js';
-import { isValidUrl } from '../lib/utils/helper.js';
+import { isValidUrl, validateFileExtension } from '../lib/utils/helper.js';
 
 export default (() => {
+  if (!arg.file || !arg.storage) {
+    throw new Error('Please specify a file path and a local storage path. see Help --help');
+  }
   const filePath = arg.file;
-readFile(filePath, 'utf8', (err, fileContent) => {
-  if (err) {
-    console.error(err);
-    return;
+
+  if(validateFileExtension(filePath, ['.txt'])) {
+    readFile(filePath, 'utf8', (err, fileContent) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const urls = fileContent.split(' ').filter((url) => isValidUrl(url));
+      if (urls.length === 0) {
+        console.log('No URLs found in the file.');
+        return;
+      }
+      batchProcessor(urls)
+    })
   }
-  const urls = fileContent.split(' ').filter((url) => isValidUrl(url));
-  if (urls.length === 0) {
-    console.log('No URLs found in the file.');
-    return;
-  }
-  batchProcessor(urls)
-})
 })()
