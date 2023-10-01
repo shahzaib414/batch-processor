8f911422c4ac45bbbf7005bdb478fc6960bf1876
Shahzaib Shahid <shahzaib.shahid414@gmail.com>
2023-10-01 21:13:52 +0200

    feat: read Urls from text file

---
+++ script.js
@@ -0,0 +1,40 @@
+const yargs = require('yargs');
+const fs = require('fs');
+
+const argv = yargs
+.option('file', {
+  alias: 'f',
+  describe: 'Specify a file path',
+  type: 'string',
+})
+.option('storage', {
+  alias: 's',
+  describe: 'Specify a local storage path',
+  type: 'string',
+})
+.help()
+.argv;
+
+const filePath = argv.file;
+const storagePath = argv.storage;
+
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+fs.readFile(filePath, 'utf8', (err, fileContent) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const urls = fileContent.split(' ').filter((url) => isValidUrl(url));
+  if (urls.length === 0) {
+    console.log('No URLs found in the file.');
+    return;
+  }
+})
